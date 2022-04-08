f50989a2e04f083422d26d39591f166f5159eb08
XrJ <49971072+jxcmdds@users.noreply.github.com>
2022-04-08 10:53:55 +0800

    Time: 0 ms (100.00%), Space: 42.7 MB (92.11%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -1,24 +1,19 @@
 class Solution {
     public int search(int[] nums, int target) {
-        //首先判断数组是否为空 和 数组长度是否为0
-        if(nums == null || nums.length == 0) {
+        // 避免当 target 小于nums[0] nums[nums.length - 1]时多次循环运算
+        if (target < nums[0] || target > nums[nums.length - 1]) {
             return -1;
         }
-
-        // 有序数组 无重复 可二分法
-        int left = 0;
-        int right = nums.length -1;
-        
-        while(left<=right){
-            int mid = (left+right)/2;
-
-            if(nums[mid]==target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] == target)
                 return mid;
-            } else if (nums[mid] < target) {
+            else if (nums[mid] < target)
                 left = mid + 1;
-            } else {
+            else if (nums[mid] > target)
                 right = mid - 1;
-            }
-        } return -1;
+        }
+        return -1;
     }
 }
